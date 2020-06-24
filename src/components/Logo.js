9e1922fc29943e87a18ccdc1e8e05484ec466601
ALONPC <alon.berrios@gmail.com>
2020-06-23 23:41:55 -0400

    init for tdd, eslint and some other packages

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+import style from "./Logo.css";
+
+export const Logo = () => (
+  <div className={"logo"}>
+    <img src={logo} alt="logo"></img>;
+  </div>
+);
